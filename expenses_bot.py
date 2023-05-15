c07368159dd2fb0f3ac224c00a9be793b1e5cbf6
AlexGrig23 <AlexGrig.Cyber@gmail.com>
2023-05-15 13:45:39 +0300

    Fixed comment

--- expenses_bot.py
+++ expenses_bot.py
@@ -11,16 +11,32 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-filename = "example.txt"
+
+categories = ["products", "entertainment", "health", "auto"]
 
 
-class Categories:
-    def __init__(self):
-        self.categories = ["products", "entertainment", "health", "auto"]
-        self.filename = filename
+class FileHandlerSingleton:
+    _instance = None
 
-    def to_dict(self):
-        return {f"{categor}": "" for categor in self.categories}
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls._instance.filename = "example.txt"
+        return cls._instance
+
+    def append_line(self, line):
+        with open(self.filename, "a") as f:
+            f.write(line)
+
+    def read_lines(self):
+        with open(self.filename, "r") as f:
+            lines = f.readlines()
+        return lines
+
+    def overwrite_file(self, lines):
+        with open(self.filename, "w") as f:
+            for line in lines:
+                f.write(line + "\n")
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -38,28 +54,21 @@ async def start(update: Update, context: CallbackContext) -> None:
                                     "/statistic <categories> [| <date_start> | <date_end>]"
                                     )
 
-"""
-Format add cat command
-/cat 
-"""
-
 
 async def cat(update: Update, context: CallbackContext) -> None:
-    new_categories_lst = Categories()
-    categories_dict = new_categories_lst.to_dict()
-    categories_string = [f"{key}\n" for key in categories_dict.keys()]
-    await update.message.reply_text("".join(categories_string))
-
-"""
-Format add add_ex command
-/add_ex <categories> | <expenses>
-"""
+    """
+    Format add cat command
+    /cat
+    """
+    await update.message.reply_text("\n".join(categories))
 
 
 async def add_ex(update: Update, context: CallbackContext) -> None:
+    """
+    Format add add_ex command
+    /add_ex <categories> | <expenses>
+    """
     await update.message.reply_text("Enter your expenses: Categories, sum of expesses")
-    ncl = Categories()
-    ncl_lst = ncl.categories
 
     ex = "cost"
     ex_parts = "".join(context.args).split("|")
@@ -67,7 +76,7 @@ async def add_ex(update: Update, context: CallbackContext) -> None:
     expen = ex_parts[1].strip()
     today = datetime.date.today()
 
-    if ex_cat not in ncl_lst:
+    if ex_cat not in categories:
         logging.error("Invalid category")
         await update.message.reply_text("You enter invalid category, check category comand '/cat'")
         return
@@ -77,10 +86,10 @@ async def add_ex(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("You enter invalid value, enter digit")
         return
 
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    if len(lines) > 0:
+    file_handler = FileHandlerSingleton()
+    lines = file_handler.read_lines()
+    print(lines)
+    if lines and len(lines) > 0:
         last_line_number = int(lines[-1].split()[0])
     else:
         last_line_number = 0
@@ -90,20 +99,18 @@ async def add_ex(update: Update, context: CallbackContext) -> None:
 
     user_data = [ex_cat, expen, today.strftime('%d.%m.%Y')]
 
-    with open(filename, "a") as f:
-        f.write(new_line + ' ' + " | ".join(user_data) + " | " + ex + "\n")
+    file_handler.append_line(new_line + ' ' + " | ".join(user_data) + " | " + ex + "\n")
     await update.message.reply_text(f"Expenses {' '.join(user_data)} append successfully")
-
-"""
-Format add list_in command
-/list_ex [<month>]
-"""
 
 
 async def list_ex(update: Update, context: CallbackContext) -> None:
+    """
+    Format add list_ex command
+    /list_ex [<month>]
+    """
     ex = "cost"
-    with open(filename, "r") as f:
-        lines = f.readlines()
+    file_handler = FileHandlerSingleton()
+    lines = file_handler.read_lines()
 
     target_month = update.message.text.strip().split()[-1]
     if target_month.isdigit():
@@ -114,31 +121,30 @@ async def list_ex(update: Update, context: CallbackContext) -> None:
     filter_lines = []
     for line in lines:
         part = line.split("|")
-        date_string = part[2].strip()
-        date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
+        if len(part) >= 3:
+            date_string = part[2].strip()
+            date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
 
-        if target_month is None and ex in line:
-            filter_lines.append(line)
+            if target_month is None and ex in line:
+                filter_lines.append(line)
 
-        elif date.month == target_month and ex in line:
-            filter_lines.append(line)
+            elif date.month == target_month and ex in line:
+                filter_lines.append(line)
 
     if filter_lines:
         await update.message.reply_text("".join(filter_lines))
     else:
         await update.message.reply_text("You don't have expenses")
-
-
-"""
-Format add list_in command
-/list_in [<month>]
-"""
 
 
 async def list_in(update: Update, context: CallbackContext) -> None:
+    """
+    Format add list_in command
+    /list_in [<month>]
+    """
     inc = "profit"
-    with open(filename, "r") as f:
-        lines = f.readlines()
+    file_handler = FileHandlerSingleton()
+    lines = file_handler.read_lines()
 
     target_month = update.message.text.strip().split()[-1]
     if target_month.isdigit():
@@ -149,27 +155,27 @@ async def list_in(update: Update, context: CallbackContext) -> None:
     filter_lines = []
     for line in lines:
         part = line.split("|")
-        date_string = part[2].strip()
-        date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
+        if len(part) >= 3:
+            date_string = part[2].strip()
+            date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
 
-        if target_month is None and inc in line:
-            filter_lines.append(line)
+            if target_month is None and inc in line:
+                filter_lines.append(line)
 
-        elif date.month == target_month and inc in line:
-            filter_lines.append(line)
+            elif date.month == target_month and inc in line:
+                filter_lines.append(line)
 
     if filter_lines:
         await update.message.reply_text("".join(filter_lines))
     else:
         await update.message.reply_text("You don't have expenses")
 
-"""
-Format add add_in command
-/add_in <categories income> | <profit>
-"""
-
 
 async def add_in(update: Update, context: CallbackContext) -> None:
+    """
+    Format add add_in command
+    /add_in <categories income> | <profit>
+    """
     await update.message.reply_text("Enter your incoming: Categories, sum incoming")
 
     inc = "profit"
@@ -189,10 +195,10 @@ async def add_in(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("You enter invalid value, enter digit")
         return
 
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    if len(lines) > 0:
+    file_handler = FileHandlerSingleton()
+    lines = file_handler.read_lines()
+    print(lines)
+    if lines and len(lines) > 0:
         last_line_number = int(lines[-1].split()[0])
     else:
         last_line_number = 0
@@ -200,65 +206,59 @@ async def add_in(update: Update, context: CallbackContext) -> None:
     new_line_number = last_line_number + 1
     new_line = f"{new_line_number}"
 
-    with open(filename, "a") as f:
-        f.write(new_line + " " + " | ".join(data_incoming) + " | " + inc + "\n")
+    file_handler.append_line(new_line + " " + " | ".join(data_incoming) + " | " + inc)
     await update.message.reply_text(f"Expenses {' '.join(data_incoming)} append successfully")
 
 
 async def list(update: Update, context: CallbackContext) -> None:
-    with open(filename, "r") as f:
-        lst = f.readlines()
-        await update.message.reply_text(" ".join(lst))
-
-
-"""
-Format add remove command
-/remove <number expensive or income>
-"""
+    file_handler = FileHandlerSingleton()
+    lines = file_handler.read_lines()
+    await update.message.reply_text(" ".join(lines))
 
 
 async def remove(update: Update, context: CallbackContext) -> None:
+    """
+    Format add remove command
+    /remove <number expensive or income>
+    """
     try:
         remove_idx = int(context.args[0])
 
-        with open(filename, "r") as f:
-            lines = f.readlines()
-            idx = False
-            for line in lines:
-                if line.startswith(str(remove_idx)):
-                    idx = True
-                    break
-            if not idx:
-                await update.message.reply_text(f"You don't have cost with number {remove_idx}")
-                return
+        file_handler = FileHandlerSingleton()
+        lines = file_handler.read_lines()
+        idx = False
+        for line in lines:
+            if line.startswith(str(remove_idx)):
+                idx = True
+                break
+        if not idx:
+            await update.message.reply_text(f"You don't have cost with number {remove_idx}")
+            return
 
-        with open(filename, "w") as f:
-            for line in lines:
-                if not line.startswith(str(remove_idx)):
-                    f.write(line)
-            await update.message.reply_text(f"You have successfully deleted costs under number {remove_idx}")
+        file_handler = FileHandlerSingleton()
+        file_handler.overwrite_file([line for line in lines if not line.startswith(str(remove_idx))])
+        await update.message.reply_text(f"You have successfully deleted costs under number {remove_idx}")
 
     except (ValueError, IndexError):
         await update.message.reply_text("You entered an invalid number or you don't entered number")
 
 
-"""
-Format add statistic command
-/statistic <categories> [| <date_start> | <date_end>]
-"""
-
-
 async def statistic(update: Update, context: CallbackContext) -> None:
+    """
+    Format add statistic command
+    /statistic <categories> [| <date_start> | <date_end>]
+    """
+
     stat_parts = "".join(context.args).split("|")
     stat_cat = stat_parts[0].strip()
     statistic_data = []
     if len(stat_parts) == 1:
-        with open(filename, "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                if stat_cat == line.split("|")[0].split()[1]:
-                    statistic_data.append(line)
-            await update.message.reply_text("".join(statistic_data))
+        file_handler = FileHandlerSingleton()
+        lines = file_handler.read_lines()
+        for line in lines:
+            if stat_cat == line.split("|")[0].split()[1]:
+                statistic_data.append(line)
+        await update.message.reply_text("".join(statistic_data))
 
     if len(stat_parts) >= 2:
         try:
@@ -271,17 +271,19 @@ async def statistic(update: Update, context: CallbackContext) -> None:
             logging.error("Invalid date format")
             await update.message.reply_text("Your date argument is not valid ")
             return
-        with open(filename, "r") as f:
-            for line in f:
-                part = line.split("|")
-                date_string = part[2].strip()
-                date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
-                if end_date is None:
-                    if start_date == date:
-                        statistic_data.append(line)
-                else:
-                    if start_date <= date <= end_date:
-                        statistic_data.append(line)
+
+        file_handler = FileHandlerSingleton()
+        lines = file_handler.read_lines()
+        for line in lines:
+            part = line.split("|")
+            date_string = part[2].strip()
+            date = datetime.datetime.strptime(date_string, "%d.%m.%Y")
+            if end_date is None:
+                if start_date == date:
+                    statistic_data.append(line)
+            else:
+                if start_date <= date <= end_date:
+                    statistic_data.append(line)
 
         await update.message.reply_text("".join(statistic_data))
 
